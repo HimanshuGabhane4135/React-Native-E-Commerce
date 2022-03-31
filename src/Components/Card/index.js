4c61fd7dfe7a6ff2996832b9758f65bcf4213d21
Himanshu <himanshug.itpath@gmail.com>
2022-03-31 15:41:22 +0530

    Card Component

---
+++ src/Components/Card/index.js
@@ -0,0 +1,27 @@
+import {Box, Center, Text} from 'native-base';
+import React from 'react';
+import {TouchableOpacity} from 'react-native';
+import {Color} from '../../Utils/color';
+
+const Card = (props, data) => {
+  console.log('data--', data);
+  return (
+    <Box>
+      <TouchableOpacity onPress={props.onPress}>
+        <Center>
+          <Text></Text>
+          <Box safeArea py="8" w="90%" maxW="500" p={5} bg={Color.gray}>
+            <Text color="white"> Name: {props.data.name}</Text>
+            <Text color="white"> Email: {props.data.email}</Text>
+            <Text color="white"> Phone No.: {props.data.phoneNo}</Text>
+            <Text color="white"> Pincode: {props.data.pincode}</Text>
+            <Text color="white"> Address: {props.data.address}</Text>
+          </Box>
+          <Text></Text>
+        </Center>
+      </TouchableOpacity>
+    </Box>
+  );
+};
+
+export default Card;

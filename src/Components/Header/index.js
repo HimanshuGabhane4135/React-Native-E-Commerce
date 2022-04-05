2ef5a6ba0af5bed907fb6e23f14d45e33b45d674
Prayag Joshi <prayagj.itpath@gmail.com>
2022-04-05 14:45:20 +0530

    SafeAreaView implementation

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Image} from 'react-native';
+import {View, Image, SafeAreaView} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import styles from './style';
 const Header = ({navigation}) => {
@@ -11,22 +11,29 @@ const Header = ({navigation}) => {
     navigation.navigate('Cart');
   };
   return (
-    <View style={styles.header}>
-      <Icon name="menu" size={28} onPress={OpenMenu} style={styles.menuIcon} />
-      <View style={styles.headerTitle}>
-        <Image
-          source={require('../../Assets/logo3.jpg')}
-          style={styles.headerImage}
-        />
-        <Icon name="search" size={28} style={styles.searchIcon} />
+    <SafeAreaView>
+      <View style={styles.header}>
         <Icon
-          name="shopping-cart"
+          name="menu"
           size={28}
-          onPress={GoToCart}
-          style={styles.cartIcon}
+          onPress={OpenMenu}
+          style={styles.menuIcon}
         />
+        <View style={styles.headerTitle}>
+          <Image
+            source={require('../../Assets/logo3.jpg')}
+            style={styles.headerImage}
+          />
+          <Icon name="search" size={28} style={styles.searchIcon} />
+          <Icon
+            name="shopping-cart"
+            size={28}
+            onPress={GoToCart}
+            style={styles.cartIcon}
+          />
+        </View>
       </View>
-    </View>
+    </SafeAreaView>
   );
 };
 
